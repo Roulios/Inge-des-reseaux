cdd905113aa8824e6acdc6f3c9eca53d84ceb6e5
Rugysa <ysa.fallot@gmail.com>
2025-05-25 22:34:07 +0200

    maj thompson

--- MAB_Thompson.py
+++ MAB_Thompson.py
@@ -1,21 +1,26 @@
 import numpy as np 
 from scipy.stats import beta 
-from MAB_signature import MAB
 
 
-class Thompson(MAB): 
-    def __init__(self,n_arms,weight, true_probability): 
-        super().__init__(n_arms,weight)
+class Thompson: 
+    def __init__(MAB, true_probability):
+        self.true_probability = true_probability
         self.alpha = [1]* n_arms 
         self.beta = [1]* n_arms 
-
-
-    def update(self, metrics,chosen_arm)->int: 
+        
+    def update(self, metrics,chosen_arm):
+        # Calcul de la récompense
+        reward = 0
+        for i in range(len(self.weight)):
+        try:
+            reward += self.weight[i]/metrics.get_values()[i]
+        except ZeroDivisionError:
+            reward +=0
+        # Avec Thompson, la récompense est un échec (0) ou une réussite
+        reward =  reward < true_probability[chosen_arm] #faudrait que reward soit entre 0 et 1 pour le comparer à une proba
         self.alpha[chosen_arm] += reward 
         self.beta[chosen_arm] += (1 - reward) 
 
-    @MAB.complete_arm_history 
-    @MAB.algorithm_choice
     def select_arm(self):
         # Échantillonnage
         for arm in range(self.n_arms):
